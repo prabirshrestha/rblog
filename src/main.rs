d2ce77ce2dcf31b5416a65a1a4709b6433a0d0ad
Prabir Shrestha <mail@prabir.me>
2020-04-25 23:20:17 -0700

    add dotenv and use port from env

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,19 @@
 use anyhow::Result;
+use std::env;
 
 #[async_std::main]
 async fn main() -> Result<()> {
+    dotenv::dotenv().ok();
+
+    let port = env::var("PORT")
+        .unwrap_or_else(|_| String::from("3000"))
+        .parse::<u16>()?;
+
+    let addr = format!("0.0.0.0:{}", port);
+
     let mut app = tide::new();
     app.at("/").get(|_| async { Ok("Hello world") });
-    app.listen("0.0.0.0:3000").await?;
+    app.listen(addr).await?;
+
     Ok(())
 }
