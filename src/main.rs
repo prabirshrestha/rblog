495d1bdbfb584448faded2e3968f9f506acbb027
Prabir Shrestha <mail@prabir.me>
2020-04-25 23:42:43 -0700

    add AppState::new_from_env()

--- src/main.rs
+++ src/main.rs
@@ -3,19 +3,28 @@ use std::env;
 use tide::{Response, StatusCode};
 
 #[derive(Debug)]
-pub struct AppState {}
+pub struct AppState {
+    addr: String,
+}
+
+impl AppState {
+    pub fn new_from_env() -> Result<Self> {
+        dotenv::dotenv().ok();
+
+        let port = env::var("PORT")
+            .unwrap_or_else(|_| String::from("3000"))
+            .parse::<u16>()?;
+        let addr = format!("0.0.0.0:{}", port);
+
+        Ok(Self { addr })
+    }
+}
 
 #[async_std::main]
 async fn main() -> Result<()> {
-    dotenv::dotenv().ok();
-
-    let port = env::var("PORT")
-        .unwrap_or_else(|_| String::from("3000"))
-        .parse::<u16>()?;
-
-    let addr = format!("0.0.0.0:{}", port);
-
-    let mut app = tide::with_state(AppState {});
+    let state = AppState::new_from_env()?;
+    let addr = state.addr.clone();
+    let mut app = tide::with_state(state);
 
     app.at("/").get(|_| async { Ok("Hello world") });
     app.at("*").all(|_| async {
