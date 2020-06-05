e529e62ddeef0c312ca3c517747c43cb93718910
Prabir Shrestha <mail@prabir.me>
2020-06-05 06:57:53 +0000

    use Body::from_bytes(buf)

--- src/routes/mod.rs
+++ src/routes/mod.rs
@@ -3,13 +3,13 @@ pub mod posts;
 pub mod rss;
 
 use crate::{appstate::AppState, templates};
-use tide::{Request, Response, StatusCode};
+use tide::{Body, Request, Response, StatusCode};
 
 pub async fn not_found(_ctx: Request<AppState>) -> tide::Result {
     let mut buf = Vec::new();
     templates::statuscode404(&mut buf)?;
 
     Ok(Response::new(StatusCode::NotFound)
-        .body_string(String::from_utf8(buf)?)
+        .body(Body::from_bytes(buf))
         .set_mime(mime::TEXT_HTML_UTF_8))
 }
