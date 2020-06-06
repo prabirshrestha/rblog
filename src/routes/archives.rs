fb1a14c7ac0aa5086779dd81b9d79bebde037717
Prabir Shrestha <mail@prabir.me>
2020-06-06 08:48:48 +0000

    add archives.rs.html

--- src/routes/archives.rs
+++ src/routes/archives.rs
@@ -1,26 +1,17 @@
-use crate::appstate::AppState;
-use itertools::Itertools;
-use tide::{http::mime, Request, Response, StatusCode};
+use crate::{appstate::AppState, blog::Post, renderer::Render, templates};
+use tide::{Request, Response, StatusCode};
 
 pub async fn get_archives(req: Request<AppState>) -> tide::Result {
     let state = &req.state();
 
-    let html = state
+    let posts: Vec<&Post> = state
         .get_blog()
         .get_all_posts()
         .map(|key| state.get_blog().get_post(key).unwrap())
-        .map(|post| {
-            String::from(format!(
-                r#"<a href="{href}"><h2>{title}</h2></a>"#,
-                href = post.get_url(),
-                title = post.get_metadata().get_title()
-            ))
-        })
-        .join("");
+        .collect();
 
     let mut res = Response::new(StatusCode::Ok);
-    res.set_body(html);
-    res.set_content_type(mime::HTML);
+    res.render_html(|o| Ok(templates::archives(o, posts)?))?;
 
     Ok(res)
 }
