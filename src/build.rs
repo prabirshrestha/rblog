349ddf6490d6369d0252cae86d69c6bdff5e1be8
Prabir Shrestha <mail@prabir.me>
2020-06-13 21:21:07 +0000

    add statics folder

--- src/build.rs
+++ src/build.rs
@@ -1,6 +1,8 @@
 use ructe::{Result, Ructe};
 
 fn main() -> Result<()> {
-    Ructe::from_env()?.compile_templates("templates")?;
+    let mut ructe = Ructe::from_env()?;
+    ructe.statics()?.add_files("static")?;
+    ructe.compile_templates("templates")?;
     Ok(())
 }
