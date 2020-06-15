574fbd5527451e48c00cb354af9a625bc2d6b4d6
Prabir Shrestha <mail@prabir.me>
2020-06-15 06:32:30 +0000

    set cache-control for assets to 1 year

--- src/routes/mod.rs
+++ src/routes/mod.rs
@@ -13,16 +13,14 @@ pub async fn not_found(_req: Request<AppState>) -> tide::Result {
 
 pub async fn get_static_file(req: Request<AppState>) -> tide::Result {
     let name = req.param::<String>("name")?;
-    println!("{}", name);
     if let Some(data) = StaticFile::get(&name) {
         let mut res = Response::new(StatusCode::Ok);
-        // TODO: add expires header
         res.set_content_type(Mime::from_str(data.mime.as_ref())?);
+        res.insert_header("cache-control", "max-age: 31536000"); // 1 year as second
         res.set_body(data.content);
         Ok(res)
     } else {
-        let mut res = Response::new(StatusCode::NotFound);
-        res.set_body(name);
+        let res = Response::new(StatusCode::NotFound);
         Ok(res)
     }
 }
