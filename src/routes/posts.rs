c90dd0d15a858d3e5f4c03cc495d608651605507
Prabir Shrestha <mail@prabir.me>
2021-09-19 13:06:11 -0700

    migrate tide to trillium

--- src/routes/posts.rs
+++ src/routes/posts.rs
@@ -1,77 +1,65 @@
-use crate::{appstate::AppState, renderer::RenderBuilder, templates};
-use tide::{Body, Redirect, Request, Response, StatusCode};
+use crate::{appstate::AppState, templates};
+use std::fs;
+use trillium::{conn_try, conn_unwrap, Conn, KnownHeaderName, Status};
+use trillium_router::RouterConnExt;
+use trillium_ructe::RucteConnExt;
 
-pub async fn get_posts(req: Request<AppState>) -> tide::Result {
-    let state = &req.state();
+pub async fn get_posts(conn: Conn) -> Conn {
+    let state = conn.state::<AppState>().unwrap().to_owned();
 
     let blog = state.get_blog();
 
     let posts = blog
         .get_all_posts()
-        .map(|key| state.get_blog().get_post(key).unwrap())
+        .map(|key| blog.get_post(key).unwrap())
         .collect();
 
-    let res = Response::builder(StatusCode::Ok)
-        .render_html(|o| Ok(templates::posts(o, blog, posts)?))?
-        .build();
-
-    Ok(res)
+    conn.render_html(|o| templates::posts_html(o, blog, posts))
 }
 
-pub async fn get_post(req: Request<AppState>) -> tide::Result {
-    let slug = req.param("slug")?;
+pub async fn get_post(conn: Conn) -> Conn {
+    let slug = conn.param("slug").unwrap_or_default();
     let normalized_slug = slug.to_lowercase();
-
     if slug != normalized_slug {
-        return Ok(Redirect::permanent(format!("/posts/{}/", &normalized_slug)).into());
+        return conn
+            .with_status(Status::PermanentRedirect)
+            .with_header(KnownHeaderName::Location, normalized_slug)
+            .halt();
     }
 
-    let state = &req.state();
+    let state = conn.state::<AppState>().unwrap().to_owned();
     let blog = state.get_blog();
 
-    if let Some(post) = blog.get_post(&slug) {
-        let res = Response::builder(StatusCode::Ok)
-            .render_html(|o| Ok(templates::post(o, blog, post)?))?
-            .build();
-        return Ok(res);
-    }
-
-    let res = Response::builder(StatusCode::NotFound)
-        .render_html(|o| Ok(templates::notfound(o)?))?
-        .build();
-    Ok(res)
+    let post = conn_unwrap!(blog.get_post(&slug), conn);
+    conn.render_html(|o| templates::post(o, blog, post))
 }
 
-pub async fn redirect_trailing_slash(req: Request<AppState>) -> tide::Result {
-    let slug = req.param("slug")?;
-    Ok(Redirect::permanent(format!("/posts/{}/", &slug)).into())
-}
-
-pub async fn get_attachment(req: Request<AppState>) -> tide::Result {
-    let slug = req.param("slug")?;
+pub async fn get_attachment(conn: Conn) -> Conn {
+    let slug = conn.param("slug").unwrap_or_default();
     let normalized_slug = slug.to_lowercase();
-
     if slug != normalized_slug {
-        return Ok(Redirect::permanent(format!("/posts/{}/", &normalized_slug)).into());
+        return conn
+            .with_status(Status::PermanentRedirect)
+            .with_header(KnownHeaderName::Location, normalized_slug)
+            .halt();
     }
 
-    let state = &req.state();
+    let state = conn.state::<AppState>().unwrap().to_owned();
     let blog = state.get_blog();
 
-    if let Some(post) = blog.get_post(&slug) {
-        let attachement_name = req.param("attachment")?;
-        println!("{}", attachement_name);
-        if let Some(attachment) = post.get_attachment(&attachement_name) {
-            let res = Response::builder(StatusCode::Ok)
-                .header("cache-control", "max-age=31536000") // 1 year as second
-                .body(Body::from_file(&attachment.get_path()).await?)
-                .build();
-            return Ok(res);
-        }
-    }
+    let post = conn_unwrap!(blog.get_post(&slug), conn);
+    let attachment_name = conn_unwrap!(conn.param("attachment"), conn);
+    let attachment = conn_unwrap!(post.get_attachment(attachment_name), conn);
 
-    let res = Response::builder(StatusCode::NotFound)
-        .render_html(|o| Ok(templates::notfound(o)?))?
-        .build();
-    Ok(res)
+    let contents = conn_try!(fs::read(attachment.get_path()), conn);
+    conn.with_status(Status::Ok)
+        .with_header(KnownHeaderName::CacheControl, "max-age=31536000") // 1 year as a second
+        .with_header(
+            KnownHeaderName::ContentType,
+            mime_guess::from_path(attachment.get_path())
+                .first_or_octet_stream()
+                .to_string(),
+        )
+        .with_body(contents)
+        .halt()
 }
