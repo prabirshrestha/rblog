ec923eb4a09b57d0d4c7478c87a9af04ae94e10b
Prabir Shrestha <mail@prabir.me>
2021-09-21 17:53:40 -0700

    remove server response header

--- src/handlers.rs
+++ src/handlers.rs
@@ -1,5 +1,10 @@
 use trillium::{Body, Conn, KnownHeaderName, Status};
 
+pub async fn remove_server_response_header(mut conn: Conn) -> Conn {
+    conn.headers_mut().remove("Server");
+    conn
+}
+
 pub async fn ensure_trailing_slash(conn: Conn) -> Conn {
     let path = conn.path().to_owned();
     if path.ends_with("/") {
