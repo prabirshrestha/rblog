add6a2c9cd61bf52008d76ad23679cf57535d795
Prabir Shrestha <mail@prabir.me>
2022-09-03 22:51:43 -0700

    add helloworld salvo

--- README.md
+++ README.md
@@ -15,6 +15,13 @@ git clone https://github.com/prabirshrestha/rblog.git
 cargo run
 ```
 
+### Running from source with listenfd
+
+```
+cargo install systemfd
+systemfd --no-pid -s http::8080 -- cargo watch -x 'run'
+```
+
 ## NGINX configuration
 
 Make sure to set the `proxy_http_version 1.1` if using nginx proxy.
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,31 @@
+use listenfd::ListenFd;
 use rblog::app::app;
+use salvo::prelude::*;
+use std::net::SocketAddr;
 
-fn main() {
+#[tokio::main]
+async fn main() {
     pretty_env_logger::init();
-    trillium_tokio::run(app());
+    // trillium_tokio::run(app());
+    serve().await;
+}
+
+#[handler]
+async fn hello_world(res: &mut Response) {
+    res.render("hello world");
+}
+
+async fn serve() {
+    let mut listenfd = ListenFd::from_env();
+    let server = if let Some(listener) = listenfd.take_tcp_listener(0).unwrap() {
+        hyper::server::Server::from_tcp(listener).unwrap()
+    } else {
+        let addr: SocketAddr = "127.0.0.1:8080"
+            .parse()
+            .expect("Unable to parse socket address");
+        hyper::server::Server::bind(&addr)
+    };
+
+    let router = Router::new().get(hello_world);
+    server.serve(Service::new(router)).await.unwrap();
 }
