3d5d96b1640d4b6ff0d7fa76af4ce0e3e51a8c26
Prabir Shrestha <mail@prabir.me>
2022-09-05 22:32:36 -0700

    migrate get_attachment to salvo

--- src/routes/posts.rs
+++ src/routes/posts.rs
@@ -1,11 +1,7 @@
 use crate::{appstate::AppState, templates};
 use anyhow::Context;
 use anyhow::Result;
-use salvo::prelude::*;
-use trillium::{conn_unwrap, Conn, KnownHeaderName, Status};
-use trillium_router::RouterConnExt;
-use trillium_ructe::RucteConnExt;
-use trillium_static::StaticConnExt;
+use salvo::{fs::NamedFile, prelude::*};
 
 #[handler]
 pub async fn get_posts(depot: &mut Depot, res: &mut Response) -> Result<()> {
@@ -48,25 +44,33 @@ pub async fn get_post(req: &mut Request, depot: &mut Depot, res: &mut Response) 
     Ok(())
 }
 
-pub async fn get_attachment(conn: Conn) -> Conn {
-    let slug = conn.param("slug").unwrap_or_default();
-    let attachment_name = conn_unwrap!(conn.param("attachment"), conn);
+#[handler]
+pub async fn get_attachment(
+    req: &mut Request,
+    depot: &mut Depot,
+    res: &mut Response,
+) -> Result<()> {
+    let slug: &str = req.param("slug").unwrap_or_default();
+    let attachment_name: &str = req.param("attachment").unwrap_or_default();
     let normalized_slug = slug.to_lowercase();
     if slug != normalized_slug {
-        let attachment_name = attachment_name.to_owned();
-        return conn
-            .with_status(Status::PermanentRedirect)
-            .with_header(
-                KnownHeaderName::Location,
-                format!("/posts/{}/{}", normalized_slug, attachment_name),
-            )
-            .halt();
+        res.redirect_temporary(format!("/posts/{}/{}", normalized_slug, attachment_name));
+        return Ok(());
     }
 
-    let state = conn.state::<AppState>().unwrap().to_owned();
+    let state = depot.obtain::<AppState>().unwrap();
     let blog = state.get_blog();
 
-    let post = conn_unwrap!(blog.get_post(slug), conn);
-    let attachment = conn_unwrap!(post.get_attachment(attachment_name), conn);
-    conn.send_path(attachment.get_path()).await
+    let post = blog.get_post(slug);
+
+    if let Some(post) = blog.get_post(slug) {
+        if let Some(attachment) = post.get_attachment(attachment_name) {
+            let file = NamedFile::open(attachment.get_path()).await?;
+            file.send(req.headers(), res).await;
+            return Ok(());
+        }
+    }
+
+    res.set_status_code(StatusCode::NOT_FOUND);
+    Ok(())
 }
