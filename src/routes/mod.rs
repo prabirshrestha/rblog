5069a6818e902a7960de656b54bf36a857377fed
Prabir Shrestha <mail@prabir.me>
2022-09-05 22:47:22 -0700

    removed old code

--- src/routes/mod.rs
+++ src/routes/mod.rs
@@ -1,11 +1,7 @@
 pub mod posts;
 pub mod rss;
 
-use crate::templates::{self, statics::StaticFile};
 use salvo::prelude::*;
-use trillium::{conn_unwrap, Conn, KnownHeaderName, Status};
-use trillium_router::RouterConnExt;
-use trillium_ructe::RucteConnExt;
 
 #[handler]
 pub async fn health_check(res: &mut Response) {
@@ -21,17 +17,4 @@ User-agent: *
 Disallow: /healthcheck
 "#,
     ))
-}
-
-pub async fn not_found(conn: Conn) -> Conn {
-    conn.render_html(|o| templates::notfound(o))
-        .with_status(Status::NotFound)
-}
-
-pub async fn get_static_file(conn: Conn) -> Conn {
-    let name = conn_unwrap!(conn.param("name"), conn);
-    let data = conn_unwrap!(StaticFile::get(name), conn);
-    conn.with_header(KnownHeaderName::ContentType, data.mime.to_string())
-        .with_header(KnownHeaderName::CacheControl, "max-age=31536000") // 1 year as second
-        .ok(data.content)
 }
