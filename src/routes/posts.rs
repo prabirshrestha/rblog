7fc507851f090dd5f68f8f7284bb65d45811b3ab
Prabir Shrestha <mail@prabir.me>
2022-09-11 09:32:04 -0700

    update slavo to 08c8918

--- src/routes/posts.rs
+++ src/routes/posts.rs
@@ -23,7 +23,10 @@ pub async fn get_post(req: &mut Request, depot: &mut Depot, res: &mut Response) 
     let slug: &str = req.param("slug").unwrap_or_default();
     let normalized_slug = slug.to_lowercase();
     if slug != normalized_slug || !req.uri().path().ends_with("/") {
-        res.render(Redirect::permanent(&format!("/posts/{}/", normalized_slug)));
+        res.render(Redirect::permanent(&format!(
+            "/posts/{}/",
+            normalized_slug
+        ))?);
         return Ok(());
     }
 
@@ -49,10 +52,10 @@ pub async fn get_attachment(
     let attachment_name: &str = req.param("attachment").unwrap_or_default();
     let normalized_slug = slug.to_lowercase();
     if slug != normalized_slug {
-        res.render(Redirect::permanent(&format!(
+        res.render(Redirect::other(&format!(
             "/posts/{}/{}",
             normalized_slug, attachment_name
-        )));
+        ))?);
         return Ok(());
     }
 
