e8e239fbb53befd6d3d8688939284efc2f642499
Prabir Shrestha <mail@prabir.me>
2023-06-08 09:09:40 -0700

    Salvo bump to v0.44.0 (#196)
    * bump salvo to v0.44.0
    
    * remove hyper dependency
    
    * rust fmt

--- src/routes/mod.rs
+++ src/routes/mod.rs
@@ -3,8 +3,7 @@ pub mod rss;
 
 use crate::templates::statics::StaticFile;
 use anyhow::{Context, Result};
-use hyper::header;
-use salvo::prelude::*;
+use salvo::{http::header, prelude::*};
 
 #[handler]
 pub async fn health_check(res: &mut Response) {
@@ -25,8 +24,8 @@ Disallow: /healthcheck
 pub async fn get_static_file(req: &mut Request, res: &mut Response) -> Result<()> {
     let name = req.param("name").context("name not found")?;
     let data = StaticFile::get(name).context("Static File not found")?;
-    res.with_header(header::CONTENT_TYPE, &data.mime.to_string(), true)?
-        .with_header(
+    res.add_header(header::CONTENT_TYPE, &data.mime.to_string(), true)?
+        .add_header(
             header::CACHE_CONTROL,
             "max-age=31536000", // 1 year as second
             true,
