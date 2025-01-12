58363fffbd05b21ead09ebe7a3b5a3ff002183c1
Prabir Shrestha <mail@prabir.me>
2025-01-12 05:43:14 -0800

    set env filter to info if not set

--- src/bin/rblog.rs
+++ src/bin/rblog.rs
@@ -2,7 +2,12 @@ use rblog::cli::Cli;
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
-    tracing_subscriber::fmt().init();
+    tracing_subscriber::fmt()
+        .with_env_filter(
+            tracing_subscriber::EnvFilter::try_from_default_env()
+                .unwrap_or_else(|_| tracing_subscriber::EnvFilter::new("info")),
+        )
+        .init();
 
     Cli::from_env().run().await?;
 
